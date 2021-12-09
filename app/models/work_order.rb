58b5281db0df5381a4de17b76489fb3ce64f2bf3
Eric Isaacson <eric@actsmedia.com>
2021-12-09 12:36:02 -0600

    basic rendering for work orders, basic styling

--- app/models/work_order.rb
+++ app/models/work_order.rb
@@ -1,4 +1,12 @@
 class WorkOrder < ApplicationRecord
   belongs_to :technician
   belongs_to :location
+
+  def pretty_tod
+    self.time.utc.strftime("%l:%M %p")
+  end
+
+  def pretty_price
+    "$%05.2f" % self.price
+  end
 end
--- app/services/schedule_service.rb
+++ app/services/schedule_service.rb
@@ -3,14 +3,26 @@ module ScheduleService
 	MINUTES_PER_HOUR = 60
 	PIXELS_Y_PER_HOUR_ROW = 75
 
+	# get an integer list representing hours valid to display on the schedule grid
+	#
 	def self.tod_range(orders)
 		first_tod = orders.map {|order| order.time.hour }.sort.first
 		last_tod = orders.map {|order| order.time.hour + order.duration / MINUTES_PER_HOUR }.sort.last
 		(first_tod..last_tod).to_a
 	end
 
+	# pretty string for integer in tod_range
+	#
 	def self.prettify_tod_hour(hour_integer)
 		Time.at(hour_integer.hours).utc.strftime("%l:%M %p")
+	end
+
+	# get css for absolute positioned schedule items
+	#
+	def self.css_for_work_order(work_order, first_tod)
+		top = (work_order.time.hour - first_tod) * PIXELS_Y_PER_HOUR_ROW
+		height = (work_order.duration.to_f / MINUTES_PER_HOUR.to_f) * PIXELS_Y_PER_HOUR_ROW
+		"top: #{ top }px; height: #{ height }px"
 	end
 
 end
