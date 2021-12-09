a514702212303097c35c7b992fc3e2d377c7076a
Eric Isaacson <eric@actsmedia.com>
2021-12-09 11:41:17 -0600

    rows per tech, each deliminating an hour

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,12 +1,9 @@
 class PublicController < ApplicationController
 
-	MINUTES_PER_HOUR = 60
-
 	def landing
 		@work_orders = WorkOrder.all
+		@technicians = Technician.all
 		@tod_range = ScheduleService::tod_range(@work_orders)
 	end
-
-
 
 end
