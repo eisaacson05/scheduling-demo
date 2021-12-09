07b16dca173681bd73349917e29c9f62fbf80bd6
Eric Isaacson <eric@actsmedia.com>
2021-12-09 11:14:32 -0600

    generate tod_range in public controller

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,4 +1,16 @@
 class PublicController < ApplicationController
+
+	MINUTES_PER_HOUR = 60
+
 	def landing
+		@work_orders = WorkOrder.all
+		@tod_range = self.tod_range(@work_orders)
 	end
+
+	def tod_range(orders)
+		first_tod = orders.map {|order| order.time.hour }.sort.first
+		last_tod = orders.map {|order| order.time.hour + order.duration / MINUTES_PER_HOUR }.sort.last
+		(first_tod..last_tod).to_a
+	end
+
 end
