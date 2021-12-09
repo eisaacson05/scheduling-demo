35d8f1bb813d853b17b425a286f64dbf04395913
Eric Isaacson <eric@actsmedia.com>
2021-12-09 14:33:00 -0600

    rendering of free time slots

--- app/models/work_order.rb
+++ app/models/work_order.rb
@@ -9,4 +9,13 @@ class WorkOrder < ApplicationRecord
   def pretty_price
     "$%05.2f" % self.price
   end
+
+  def css_top(first_hour)
+    (self.time.hour - first_hour) * ScheduleService::PIXELS_Y_PER_HOUR_ROW
+  end
+
+  def css_height
+    (self.duration.to_f / ScheduleService::MINUTES_PER_HOUR.to_f) * ScheduleService::PIXELS_Y_PER_HOUR_ROW
+  end
+
 end
--- app/services/schedule_service.rb
+++ app/services/schedule_service.rb
@@ -17,12 +17,48 @@ module ScheduleService
 		Time.at(hour_integer.hours).utc.strftime("%l:%M %p")
 	end
 
-	# get css for absolute positioned schedule items
+	# get css for absolute positioned work orders
 	#
 	def self.css_for_work_order(work_order, first_tod)
 		top = (work_order.time.hour - first_tod) * PIXELS_Y_PER_HOUR_ROW
 		height = (work_order.duration.to_f / MINUTES_PER_HOUR.to_f) * PIXELS_Y_PER_HOUR_ROW
-		"top: #{ top }px; height: #{ height }px"
 	end
 
+	# get css for absolute positioned free times
+	#
+	def self.free_times(work_orders, first_hour)
+		work_orders = work_orders.sort_by  { |item| item.time}
+		[
+			self.free_time_before_orders(work_orders.first, first_hour),
+			*self.free_times_between_orders(work_orders, first_hour),
+			self.free_time_after_orders(work_orders.last, first_hour)
+		]
+	end
+
+	def self.free_time_before_orders(first_order, first_hour)
+		height= first_order.css_top(first_hour)
+		{
+			css: "top: 0px; height: #{ height }px",
+			time_between_orders: "" # TODO
+		}
+	end
+
+	def self.free_times_between_orders(work_orders, first_hour)
+		work_orders.each_cons(2).map do | order_prev, order |
+			top = order_prev.css_top(first_hour) + order_prev.css_height
+			height = order.css_top(first_hour) - top
+			{
+				css: "top: #{ top }px; height: #{ height }px",
+				time_between_orders: "" # TODO
+			}
+		end
+	end
+
+	def self.free_time_after_orders(last_order, first_hour)
+		top = last_order.css_top(first_hour) + last_order.css_height
+		{
+			css: "top: #{ top }px; bottom: 0px",
+			time_between_orders: "" # TODO
+		}
+	end
 end
